6fbb42fe94e1e2f064622023c68c48cc9e9e05a7
yangen <yang_sen_ysys@163.com>
2018-05-06 02:14:28 +0800

    first

---
+++ YSView/YSView/UIView+YSDrag.h
@@ -0,0 +1,13 @@
+//
+//  UIView+YSDrag.h
+//  YSView
+//
+//  Created by 杨森 on 2018/5/5.
+//  Copyright © 2018年 yangsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (YSDrag)
+
+@end
---
+++ YSView/YSView/UIView+YSDrag.m
@@ -0,0 +1,13 @@
+//
+//  UIView+YSDrag.m
+//  YSView
+//
+//  Created by 杨森 on 2018/5/5.
+//  Copyright © 2018年 yangsen. All rights reserved.
+//
+
+#import "UIView+YSDrag.h"
+
+@implementation UIView (YSDrag)
+
+@end
